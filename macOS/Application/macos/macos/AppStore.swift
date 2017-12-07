bcf3492ad0d6c7865d560b6701ccbb0b270bbf72
keenaudio <aaron@keenaudio.com>
2017-12-07 15:25:39 -0700

    reswift dep

---
+++ macOS/Application/macos/macos/AppStore.swift
@@ -0,0 +1,15 @@
+//
+//  AppStore.swift
+//  macos
+//
+//  Created by Aaron Granick on 12/7/17.
+//  Copyright © 2017 Aaron Granick. All rights reserved.
+//
+
+import Foundation
+
+import ReSwift
+
+struct AppState: StateType {
+    var counter: Int = 0
+}
