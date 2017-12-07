6c62cd5b54c5cb5da5cd812139bdb5f33ae4eb67
keenaudio <aaron@keenaudio.com>
2017-12-07 14:30:35 -0700

    supermon (macOs) initial commit

---
+++ macOS/Application/macos/macos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macos
+//
+//  Created by Aaron Granick on 12/7/17.
+//  Copyright © 2017 Aaron Granick. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
